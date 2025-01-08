82448512dd23d5e4da044abdb0817b5e8e5b218d
Roberta Angioni <93215385+Berthaji@users.noreply.github.com>
2025-01-08 15:50:44 +0100

    Preprocessed dataset

--- label_dataset.py
+++ label_dataset.py
@@ -2,25 +2,39 @@ import os
 import pandas as pd
 
 # Percorsi delle directory
-happy_dir_train = "FER2013/train/happy"
-sad_dir_train = "FER2013/train/sad"
+happy_dir = "FER2013/test/happy"
+sad_dir = "FER2013/test/sad"
 
 # Creazione di liste per memorizzare i dati
 data = []
+happy_data = []
+sad_data = []
 
 # Itera sui file nella directory "happy" e assegna l'etichetta 1
-for file_name in os.listdir(happy_dir_train):
+for file_name in os.listdir(happy_dir):
     data.append([file_name, 1])
+    happy_data.append([file_name, 1])  # Aggiungi solo i file happy
 
 # Itera sui file nella directory "sad" e assegna l'etichetta 0
-for file_name in os.listdir(sad_dir_train):
+for file_name in os.listdir(sad_dir):
     data.append([file_name, 0])
+    sad_data.append([file_name, 0])  # Aggiungi solo i file sad
 
-# Creazione di un DataFrame
-df = pd.DataFrame(data, columns=["Image_Name", "Label"])
+# Creazione di DataFrame
+df_all = pd.DataFrame(data, columns=["Image_Name", "Label"])
+df_happy = pd.DataFrame(happy_data, columns=["Image_Name", "Label"])
+df_sad = pd.DataFrame(sad_data, columns=["Image_Name", "Label"])
 
-# Salvataggio in un file CSV
-output_path = "image_labels_train.csv"
-df.to_csv(output_path, index=False)
+# Salvataggio dei file CSV
+output_all_path = "all_image_labels.csv"
+output_happy_path = "happy_labels.csv"
+output_sad_path = "sad_labels.csv"
 
-print(f"File CSV creato con successo: {output_path}")
+df_all.to_csv(output_all_path, index=False)
+df_happy.to_csv(output_happy_path, index=False)
+df_sad.to_csv(output_sad_path, index=False)
+
+print(f"File CSV creati con successo:")
+print(f"  - Happy e sad: {output_all_path}")
+print(f"  - Solo happy: {output_happy_path}")
+print(f"  - Solo sad: {output_sad_path}")
